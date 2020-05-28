cfdfa3dc6b02cee69addc58cea739c21102c2643
Robinkariuki <robinkaris7@gmail.com>
2020-05-28 14:22:58 +0300

    added poll reducer

--- src/store/reducers/pollReducers.js
+++ src/store/reducers/pollReducers.js
@@ -1,3 +1,35 @@
+import {Vote_PC,Vote_Playstation,Vote_Xbox} from './actionsType';
 
 
 
+const initialState ={
+    playstation:0,
+    Xbox:0,
+    PC:0,
+}
+
+
+const pollReducer=(state=initialState,action)=>{
+    switch(action.type){
+        case Vote_Playstation:
+            console.log('Your choice is Playstation!')
+            return Object.assign({},state,{playstation:state.playstation+1})
+        
+        
+        case Vote_Xbox:
+            console.log('Your choice is Xbox!')
+            return Object.assign({},state,{Xbox:state.Xbox+1})
+        
+        case Vote_PC:
+             console.log('Your choice is PC!')
+             return Object.assign({},state,{PC:state.PC+1})    
+        
+    default:
+        return state
+    }
+        
+}
+
+
+
+export default pollReducer;
