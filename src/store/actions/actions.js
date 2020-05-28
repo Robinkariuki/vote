a64ef67c9bb45945894632fa2249eb13ff6b41d9
Robinkariuki <robinkaris7@gmail.com>
2020-05-28 14:07:43 +0300

    added actions and action types

---
+++ src/store/actions/actions.js
@@ -0,0 +1,16 @@
+import {Vote_PC,Vote_Playstation,Vote_Xbox} from './actionsType';
+
+const Voteplaystation =()=>{
+    type = Vote_Playstation
+}
+
+const VotePc =()=>{
+    type = Vote_PC
+}
+
+const VoteXbox =()=>{
+    type = Vote_Xbox
+}
+
+
+export default {Voteplaystation,VoteXbox,VotePc};
---
+++ src/store/actions/actionsType.js
@@ -0,0 +1,3 @@
+export const Vote_PC = 'Vote_PC';
+export const Vote_Playstation = 'Vote_Playstation';
+export const Vote_Xbox ='Vote_Xbox';
