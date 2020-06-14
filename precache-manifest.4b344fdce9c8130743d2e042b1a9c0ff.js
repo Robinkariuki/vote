a9468111a90b05bc54a2933b809f79f41f7083b0
Robinkariuki <robinkaris7@gmail.com>
2020-06-14 13:12:49 +0300

    Updates

---
+++ precache-manifest.4b344fdce9c8130743d2e042b1a9c0ff.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16575a9938f98162310aff2ad9250a93",
+    "url": "/vote/index.html"
+  },
+  {
+    "revision": "85ea4a4f3cd326c34efd",
+    "url": "/vote/static/js/2.b9c20aff.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/vote/static/js/2.b9c20aff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4dce132eea102c5cefb",
+    "url": "/vote/static/js/main.92d2710d.chunk.js"
+  },
+  {
+    "revision": "59002d512ad3528eaab9",
+    "url": "/vote/static/js/runtime-main.a9c824b8.js"
+  },
+  {
+    "revision": "090cca5f45cab75d3a887c154fd286bb",
+    "url": "/vote/static/media/Ps.090cca5f.jpeg"
+  },
+  {
+    "revision": "db1755fffe6e3b6ea684855034773476",
+    "url": "/vote/static/media/Xbox.db1755ff.jpg"
+  },
+  {
+    "revision": "a96f4f3c2585d125ffd579379fe0ac19",
+    "url": "/vote/static/media/con.a96f4f3c.png"
+  },
+  {
+    "revision": "b623dc1d11ceaa4a24975ff204e4952a",
+    "url": "/vote/static/media/pc.b623dc1d.jpeg"
+  }
+]);
